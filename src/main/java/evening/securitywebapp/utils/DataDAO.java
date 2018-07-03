da1b7886ec056a6b5de0c5727fb42b13088a1362
Dragos Sarpe <contact@usdgadget.com>
2018-07-03 13:06:11 +0300

    Initial import of Java project

---
+++ src/main/java/evening/securitywebapp/utils/DataDAO.java
@@ -0,0 +1,39 @@
+package evening.securitywebapp.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import evening.securitywebapp.bean.UserAccount;
+  
+public class DataDAO {
+ 
+   private static final Map<String, UserAccount> mapUsers = new HashMap<String, UserAccount>();
+ 
+   static {
+      initUsers();
+   }
+ 
+   private static void initUsers() {
+ 
+      // This user has a role as EMPLOYEE.
+      UserAccount emp = new UserAccount("employee1", "123", UserAccount.GENDER_MALE, //
+            SecurityConfig.ROLE_EMPLOYEE);
+ 
+      // This user has 2 roles EMPLOYEE and MANAGER.
+      UserAccount mng = new UserAccount("manager1", "123", UserAccount.GENDER_MALE, //
+            SecurityConfig.ROLE_EMPLOYEE, SecurityConfig.ROLE_MANAGER);
+ 
+      mapUsers.put(emp.getUserName(), emp);
+      mapUsers.put(mng.getUserName(), mng);
+   }
+ 
+   // Find a User by userName and password.
+   public static UserAccount findUser(String userName, String password) {
+      UserAccount u = mapUsers.get(userName);
+      if (u != null && u.getPassword().equals(password)) {
+         return u;
+      }
+      return null;
+   }
+ 
+}
